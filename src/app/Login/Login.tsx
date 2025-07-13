5c7aad364d42011e81f837b3d8892b937009be9d
“Shelton <damwcs@gmail.com>
2025-07-13 21:15:45 +1000

    finish login page structure

--- src/app/Login/Login.tsx
+++ src/app/Login/Login.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import { Link } from 'react-router';
+import { Button, Form } from 'react-bootstrap';
+import { useState } from 'react';
 
-const Login: React.FC = () => {
+interface FormData {
+  username: string;
+  password: string;
+}
+
+const Login = () => {
+  const [formData, setFormData] = useState<FormData>({
+    username: '',
+    password: '',
+  });
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-      <p>This is the Login component</p>
+    <div className='d-flex justify-content-center align-items-center vh-100'>
+      <Form
+        className='d-flex flex-column gap-3'
+        style={{ width: '400px' }}
+        onSubmit={handleSubmit}
+      >
+        <h1 className='align-self-center fs-3 '>Welcome Back</h1>
+        <Form.Group controlId='username'>
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter your email'
+            value={formData.username}
+            onChange={e =>
+              setFormData({ ...formData, username: e.target.value })
+            }
+          />
+        </Form.Group>
+        <Form.Group controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter your password'
+            value={formData.password}
+            onChange={e =>
+              setFormData({ ...formData, password: e.target.value })
+            }
+          />
+        </Form.Group>
+        <Button type='submit'>Log in</Button>
+        <Link to='/register' className='align-self-center'>
+          Don't have an account? Register
+        </Link>
+      </Form>
     </div>
   );
 };
